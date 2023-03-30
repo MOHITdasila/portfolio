6c9f0baee851b8955a7e37e3b5587b52ff260e70
MOHITdasila <dasila.m24@gmail.com>
2023-03-30 13:01:35 +0530

    header-portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,50 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatIconModule } from '@angular/material/icon';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
+// import { UsersComponent } from './users/users.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from
+  '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent
   ],
+  // imports: [
+  //   BrowserModule,
+  //   AppRoutingModule,
+  //   MatToolbarModule,
+  //   MatIconModule
+  // ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
